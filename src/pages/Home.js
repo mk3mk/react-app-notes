ad9677b3ed9aa8cb8b605cefd9e726912adef86c
mk3mk <mk3mk@yandex.ru>
2020-03-15 14:42:54 +0300

    444

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, {Fragment, useContext, useEffect} from 'react'
+import {Form} from '../components/Form'
+
+
+export const Home = () => {
+  return (
+    <Fragment>
+      
+      <h1>Home page</h1>
+
+      <Form />
+      
+    </Fragment>
+  )
+}
+
+
